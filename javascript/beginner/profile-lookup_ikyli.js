33dbb70aa48b8e99766dc07ee2dfb0f6d9f54484
kylifoo <kyli.foo@photobookworldwide.com>
2019-10-22 14:04:14 +0800

    profile look up by kyli

--- javascript/beginner/profile-lookup_ikyli.js
+++ javascript/beginner/profile-lookup_ikyli.js
@@ -1,4 +1,4 @@
-contacts = [
+var contacts = [
   {
     firstName: "Akira",
     lastName: "Laine",
@@ -24,3 +24,23 @@ contacts = [
     likes: ["JavaScript", "Gaming", "Foxes"]
   }
 ];
+
+function look_up_profile(name, info) {
+  contacts.forEach(function(contacts) {
+    if (name == contacts.firstName) {
+      if (info == "lastName".toLowerCase) {
+        return console.log(contacts.lastName);
+      }
+      if (info == "number".toLowerCase) {
+        return console.log(contacts.number);
+      }
+      if (info == "likes".toLowerCase) {
+        return console.log(contacts.likes);
+      }
+    } else {
+      console.log("No contact founded");
+    }
+  });
+}
+
+look_up_profile("Akira", "likes");
